9d07c370eb56ce298a0e52b65f5f3c6ba2adc344
jaysmith2019 <jsmith@paradymemanagement.com>
2019-01-24 11:08:51 -0500

    hello world change

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var express = require('express'),
+    fs = require('fs'),
+    app = express();
+ 
+var app = express();
+ 
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+ 
+ 
+app.get('/', function(req, res) {
+    res.send('Hello from NodeJS  at '+ new Date());
+});
+ 
+ 
+app.listen(8080, ip);
+ 
+module.exports = app;
