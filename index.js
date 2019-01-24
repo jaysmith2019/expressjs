0e41877da83ac0bdf7f098b3cc153459468ae7cb
jaysmith2019 <jsmith@paradymemanagement.com>
2019-01-24 11:22:13 -0500

    adding cumulus to project

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
  
 app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date());
+    res.sendFile('./index.html');
 });
  
  
