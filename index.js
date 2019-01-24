7da48ffe86302be4f41dbe8f5e98a4c08c69eee8
jaysmith2019 <jsmith@paradymemanagement.com>
2019-01-24 12:10:38 -0500

    update to include working cumulus

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-var express = require('express'),
-    fs = require('fs'),
-    app = express();
- 
+var express = require('express');
 var app = express();
- 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
- 
- 
-app.get('/', function(req, res) {
-    res.sendFile('./index.html');
+var path = require('path');
+
+app.use(express.static('static'))
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/static/index.html'));
 });
- 
- 
-app.listen(8080, ip);
- 
-module.exports = app;
+
+app.listen(8080, port);
